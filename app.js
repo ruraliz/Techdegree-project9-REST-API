7efd2a6c68c8137a965c5f2975a81ccd7abab4e9
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-10-18 15:01:17 -0700

    created user and course models with necessary associations

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const { sequelize } = require('./models');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+
+// Setup request body JSON parsing.
+app.use(express.json());
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
@@ -42,7 +46,23 @@ app.use((err, req, res, next) => {
 // set our port
 app.set('port', process.env.PORT || 5000);
 
+//Test the database connection 
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
 // start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
-});
+// const server = app.listen(app.get('port'), () => {
+//   console.log(`Express server is listening on port ${server.address().port}`);
+// });
+sequelize.sync()
+  .then( () => {
+    const server = app.listen(app.get('port'), () => {
+      console.log(`Express server is listening on port ${server.address().port}`);
+    });
+  });
---
+++ models/course.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Course extends Model {}
+    Course.init({
+        title:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notNull:{
+                    msg: 'Must provide a title'
+                },
+                notEmpty:{
+                    msg: 'Please provide a title'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate:{
+                notNull:{
+                    msg: 'Must provide a description'
+                },
+                notEmpty:{
+                    msg: 'Please provide a course description'
+                }
+            }
+        },
+        estimatedTime:{
+            type: DataTypes.STRING,
+        },
+        materialsNeeded:{
+            type: DataTypes.STRING,
+        },
+    }, {sequelize})
+    Course.associate = (models) =>{
+        Course.belongsTo(models.User, {
+            as: 'courseUser',
+            foreignKey:{
+                fieldName: 'userId'
+            }
+        })
+    }
+    return Course;
+}
