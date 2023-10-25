a044a0362b5ecae49373e2ba174cd5743562ad9d
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-10-25 15:32:28 -0700

    updated app.js

--- app.js
+++ app.js
@@ -61,9 +61,6 @@ app.set('port', process.env.PORT || 5000);
 })();
 
 // start listening on our port
-// const server = app.listen(app.get('port'), () => {
-//   console.log(`Express server is listening on port ${server.address().port}`);
-// });
 sequelize.sync()
   .then( () => {
     const server = app.listen(app.get('port'), () => {
